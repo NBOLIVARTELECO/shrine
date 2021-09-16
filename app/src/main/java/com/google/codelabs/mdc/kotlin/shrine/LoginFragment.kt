ff98493bb42a835a8cabdc560a114669ede1ce1d
NBOLIVARTELECO <45df54VB@>
2021-09-15 19:12:39 -0500

    Id textFields Navigation to ProductFragment

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -17,8 +20,33 @@ class LoginFragment : Fragment() {
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
+
         val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        view.next.setOnClickListener({
+            if (!passValidation(pass_edit_text.text!!)) {
+                pass_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                pass_text_input.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        })
+
+        // Clear the error once more than 8 characters are typed.
+        view.pass_edit_text.setOnKeyListener({ _, _, _ ->
+            if (passValidation(pass_edit_text.text!!)) {
+                // Clear the error.
+                pass_text_input.error = null
+            }
+            false
+        })
+
         return view
+    }
+
+    private fun passValidation(text:Editable): Boolean{
+        return text != null && text.length >= 5
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
